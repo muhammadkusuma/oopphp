a54871489129127d8b76f23801103a5f62290f9c
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-12-09 11:27:59 +0700

    visibility

---
+++ visibility.php
@@ -0,0 +1,95 @@
+<?php
+// Jualan Produk
+// Komik
+// Game
+
+class Produk
+{
+    public $judul,
+        $penulis,
+        $penerbit;
+
+    protected $harga;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+    {
+        $this->judul = $judul;
+        $this->penulis = $penulis;
+        $this->penerbit = $penerbit;
+        $this->harga = $harga;
+    }
+
+    public function getLabel()
+    {
+        return "$this->penulis, $this->penerbit";
+    }
+
+    public function getInfoProduct()
+    {
+        // Komik : Naruto | Masashi Kishimoto, Shonen Jump (Rp. 30000) - 100 Halaman.
+        $str = " {$this->judul} | {$this->getLabel()} (Rp. {$this->harga})";
+
+        return $str;
+    }
+}
+
+class Komik extends Produk
+{
+    public $jmlHalaman;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+        $this->jmlHalaman = $jmlHalaman;
+    }
+
+    public function getInfoProduct()
+    {
+        $str = "Komik : " .  parent::getInfoProduct() . " - {$this->jmlHalaman} Halaman.";
+        return $str;
+    }
+}
+
+class Game extends Produk
+{
+    public $waktuMain;
+
+    public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0)
+    {
+        parent::__construct($judul, $penulis, $penerbit, $harga);
+        $this->waktuMain = $waktuMain;
+    }
+
+    public function getHarga()
+    {
+        return $this->harga;
+    }
+
+
+    public function getInfoProduct()
+    {
+        $str = "Game : " .  parent::getInfoProduct() . " ~ {$this->waktuMain} Jam.";
+        return $str;
+    }
+}
+
+
+class CetakInfoProduk
+{
+    public function cetak(Produk $produk)
+    {
+        $str = "{$produk->judul} | {$produk->getLabel()} (Rp. {$produk->harga})";
+        return $str;
+    }
+}
+
+
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 =  new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
+
+echo $produk1->getInfoProduct();
+echo "<br>";
+echo $produk2->getInfoProduct();
+echo "<br>";
+
+echo $produk2->getHarga();

b6d3b5800cf478310b5af0e306c6490d0bf9e63c
muhammadkusuma <mwiraadekusuma1@gmail.com>
2022-12-10 14:00:30 +0700

    namespace

---
+++ namespace/App/init.php
@@ -0,0 +1,14 @@
+<?php 
+
+spl_autoload_register(function ($class){
+    $class = explode('\\', $class);
+    $class = end($class);
+    require __DIR__. '/Produk/' . $class . '.php';
+});
+
+spl_autoload_register(function ($class){
+    $class = explode('\\', $class);
+    $class = end($class);
+    require __DIR__. '/Service/' . $class . '.php';
+});
+?>
